dceac89055f82dcc2390773d7cf63bd03b08556b
RinghettiFranco <ringhetti.franco@gmail.com>
2022-06-30 11:23:20 -0300

    Primer instancia del filtrado y fetch de los datos

--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -4,30 +4,18 @@ import { useGlobalContext } from '../context'
 const Filtro = () => {
   const { marcas, productos, formatos, setFilterType, setFilterTerm } = useGlobalContext()
 
+  const handleChange = (e) => {
+    setFilterTerm(e.target.value)
+  }
+
   return (
     <section className="section search">
       <form className="search-form">
         <label htmlFor="marca">Marca</label>
-        <select id="marca">
+        <select id="marca" onChange={(e) => handleChange(e)}>
           <option value="0" disabled selected> Ninguna</option>
           {marcas.map((item) => {
             return <option value={item.id}> {item.nombre} </option>
-          })}
-        </select>
-        
-        <label htmlFor="producto">Producto</label>
-        <select id="producto">
-          <option value="0" disabled selected> Ninguno</option>
-          {productos.map((item) => {
-            return <option value={item.id}> {item.nombre} </option>
-          })}
-        </select>
-        
-        <label htmlFor="formato">Formato</label>
-        <select id="formato">
-          <option value="0" disabled selected> Ninguno</option>
-          {formatos.map((item) => {
-            return <option value={item.id}> {item.descripcion} {item.cantidad} {item.medidas} </option>
           })}
         </select>
       </form>
--- src/context.js
+++ src/context.js
@@ -1,9 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useCallback } from 'react'
-import formatosPre from './datos/formatosPre.js'
-import marcasPre from './datos/marcasPre.js'
-import productosPre from './datos/productosPre.js'
-import presentacionesPre from './datos/presentacionesPre.js'
 
 const URL_PRESENTACIONES = 'http://prlvl-distribuidora.herokuapp.com/api/presentaciones'
 const URL_MARCAS = 'http://prlvl-distribuidora.herokuapp.com/api/marcas'
@@ -17,39 +13,65 @@ const FORMATO_FILTRO = 3
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [filterType, setFilterType] = useState(0)
   const [filterTerm, setFilterTerm] = useState(0)
-  const [presentaciones, setPresentaciones] = useState(presentacionesPre)
-  const [marcas, setMarcas] = useState(marcasPre)
-  const [formatos, setFormatos] = useState(formatosPre)
-  const [productos, setProductos] = useState(productosPre)
+  const [presentaciones, setPresentaciones] = useState([])
+  const [marcas, setMarcas] = useState([])
+  const [formatos, setFormatos] = useState([])
+  const [productos, setProductos] = useState([])
 
-  /**
-  const fetchMarcas = fetch(URL_MARCAS);
-  fetchMarcas.then(response => {
-    return response.json();
-  }).then(data => {
-    console.log(data);
-    setMarcas(data);
-  });
+  useEffect(() => {
+    const fetchMarcas = fetch(URL_MARCAS);
+    fetchMarcas.then(response => {
+      return response.json();
+    }).then(data => {
+      const marcasActuales = data.marcas;
+      setMarcas(marcasActuales);
+    });
 
-  const fetchFormatos = fetch(URL_FORMATOS);
-  fetchFormatos.then(response => {
-    return response.json();
-  }).then(data => {
-    console.log(data);
-    setMarcas(data);
-  });
+    const fetchFormatos = fetch(URL_FORMATOS);
+    fetchFormatos.then(response => {
+      return response.json();
+    }).then(data => {
+      const formatosActuales = data.formatos;
+      setFormatos(formatosActuales);
+    });
 
-  const fetchProductos = fetch(URL_PRODUCTOS);
-  fetchProductos.then(response => {
-    return response.json();
-  }).then(data => {
-    console.log(data);
-    setMarcas(data);
-  });
-  **/
+    const fetchProductos = fetch(URL_PRODUCTOS);
+    fetchProductos.then(response => {
+      return response.json();
+    }).then(data => {
+      const productosActuales = data.productos;
+      setProductos(productosActuales);
+    });
+
+    const fetchPresentaciones = fetch(URL_PRESENTACIONES);
+    fetchPresentaciones.then(response => {
+      return response.json();
+    }).then(data => {
+      const presentacionesActuales = data.presentaciones;
+      setPresentaciones(presentacionesActuales);
+    });
+
+    setLoading(false);
+  }, [])
+
+  useEffect(() => {
+    setLoading(true);
+    //SOLO MARCAS POR AHORA
+    if(filterTerm !== 0){
+      const fetchPresentacionesFiltradas = fetch(URL_MARCAS+'/'+filterTerm);
+      fetchPresentacionesFiltradas.then(response => {
+        return response.json();
+      }).then(data => {
+        const presentacionesFiltradas = data.presentaciones;
+        setPresentaciones(presentacionesFiltradas);
+      });
+    }
+
+    setLoading(false);
+  },[filterTerm])
 
   return <AppContext.Provider value={{
     loading,
@@ -65,7 +87,6 @@ const AppProvider = ({ children }) => {
   }}>{children}</AppContext.Provider>
 }
 
-// make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
